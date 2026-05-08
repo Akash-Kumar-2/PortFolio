70d274029ee24858e2f2be0534dbd3005c2eb630
Akash-Kumar-2 <yka334@gmail.com>
2026-05-08 20:47:59 +0530

    Added lazy loading

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'three-core': ['three'],
+          'three-fiber': ['@react-three/fiber', '@react-three/drei'],
+          'gsap': ['gsap', '@gsap/react'],
+          'globe': ['react-globe.gl'],
+          'spring': ['react-spring', 'react-spring-3d-carousel'],
+        },
+      },
+    },
+  },
 })
